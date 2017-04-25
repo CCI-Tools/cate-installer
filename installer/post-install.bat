99bca336770fc2b3cca568b1b74b7d91e9895ace
Marco Zuehlke <Marco.Zuehlke@brockmann-consult.de>
2017-04-25 10:52:08 +0200

    0.8.0rc1, WEB_API 0.8

--- installer/post-install.bat
+++ installer/post-install.bat
@@ -1,3 +1,3 @@
-set VERSION=0.7
+set VERSION=0.8
 mkdir "%USERPROFILE%\.cate\%VERSION%"
 echo %PREFIX% > %USERPROFILE%\.cate\%VERSION%\cate.location
--- installer/post-install.sh
+++ installer/post-install.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
-VERSION="0.7"
+VERSION="0.8"
 mkdir -p ${HOME}/.cate/${VERSION}
 echo ${PREFIX} > ${HOME}/.cate/${VERSION}/cate.location

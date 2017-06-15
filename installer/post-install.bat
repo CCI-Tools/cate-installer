acaad701ad2742e2096bdb5eb80585add95ad1d6
Norman Fomferra <norman.fomferra@brockmann-consult.de>
2017-06-15 16:29:35 +0200

    0.8.0rc5

--- installer/post-install.bat
+++ installer/post-install.bat
@@ -1,4 +1,4 @@
-set VERSION=0.8.0rc3
+set VERSION=0.8.0rc5
 set VERSION_DIR=%USERPROFILE%\.cate\%VERSION%
 mkdir "%VERSION_DIR%"
 echo %PREFIX% > "%VERSION_DIR%\cate.location"
--- installer/post-install.sh
+++ installer/post-install.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-VERSION="0.8.0rc3"
+VERSION="0.8.0rc5"
 VERSION_DIR="${HOME}/.cate/${VERSION}"
 
 mkdir -p ${VERSION_DIR}

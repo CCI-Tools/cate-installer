bebbf4e12722405b75b755330f8ef6028a9732d5
Norman Fomferra <norman.fomferra@gmail.com>
2018-02-08 08:49:16 +0100

    1.1.0-dev1

--- installer/post-install.bat
+++ installer/post-install.bat
@@ -1,4 +1,5 @@
-set VERSION=1.0.0
+rem Use PEP 440 compliant version identifier
+set VERSION=1.1.0.dev1
 set VERSION_DIR=%USERPROFILE%\.cate\%VERSION%
 mkdir "%VERSION_DIR%"
 echo %PREFIX% > "%VERSION_DIR%\cate.location"
--- installer/post-install.sh
+++ installer/post-install.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 
-VERSION="1.0.0"
+# Use PEP 440 compliant version identifier
+VERSION="1.1.0.dev1"
 VERSION_DIR="${HOME}/.cate/${VERSION}"
 
 mkdir -p ${VERSION_DIR}

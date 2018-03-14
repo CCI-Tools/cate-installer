91b3ccbbf639cc5471b9b86f2b37669737673a48
Norman Fomferra <norman.fomferra@gmail.com>
2018-03-14 10:03:49 +0100

    2.0.0-dev.3, b1

--- installer/post-install.bat
+++ installer/post-install.bat
@@ -1,5 +1,5 @@
 rem Use PEP 440 compliant version identifier
-set VERSION=2.0.0.dev2
+set VERSION=2.0.0.dev3
 set VERSION_DIR=%USERPROFILE%\.cate\%VERSION%
 mkdir "%VERSION_DIR%"
 echo %PREFIX% > "%VERSION_DIR%\cate.location"
